73b93fe67a3a981351a4189b806c8dab2fa8d81f
3bdo555 <3bode13579@gmail.com>
2025-12-08 20:39:17 +0200

    Add titanic dataset

--- casestudy.py
+++ casestudy.py
@@ -4,13 +4,14 @@ import kagglehub
 from kagglehub import KaggleDatasetAdapter
 
 # Set the path to the file you'd like to load
-file_path = "avg-household-size.csv"
+file_path = "Titanic-Dataset.csv"
 
 # Load the latest version
 df = kagglehub.load_dataset(
   KaggleDatasetAdapter.PANDAS,
-  "thedevastator/uncovering-trends-in-health-outcomes-and-socioec",
+  "yasserh/titanic-dataset",
   file_path,
+ 
 )
 
 print("First 5 records:", df.head())
